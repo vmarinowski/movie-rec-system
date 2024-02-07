4b8d5d54c81b06e2f17aa1e85377f91482b130c0
Nureddin Aliyev <112823694+vmarinowski@users.noreply.github.com>
2024-02-07 21:44:22 +0400

    Update data.py

--- data.py
+++ data.py
@@ -4,7 +4,7 @@ import numpy as np
 df = pd.read_csv("./data/embeddings.csv")
 embeddings = df.values
 
-movies = pd.read_csv("./data/movies.csv")
+#movies = pd.read_csv("./data/movies.csv")
 
 with open("./data/keywords.txt", 'r', encoding = 'utf-8') as text:
     keywords = [x.strip() for x in text.readlines()]
